75ca1845add91fc285d6087799cd2372bf3dc542
Aparna <manikandanaparna18@gmail.com>
2024-04-24 10:16:04 +0530

    Feat:admin dashboard

--- pm2.config.js
+++ pm2.config.js
@@ -5,5 +5,23 @@ module.exports = {
     env: {
         Node_ENV: 'production',
         PORT: 3000,
+        MONGO_URI='mongodb+srv://aparnapie2547:ElysianBook@elysianbook.ntkhmlz.mongodb.net/ElysianBook?retryWrites=true&w=majority',
+EMAIL_PASS = oieg anhq chqh qemz,
+
+Node_ENV = production,
+
+JWT_KEY = jwt123,
+REFRESH_TOKEN_SECRET=refresh123,
+
+
+CLOUDINARY_CLOUD=dr2avntwv,
+CLOUDINARY_API_KEY=829874218365388,
+CLOUDINARY_API_SECRET=cZ_7TCeKD4mJf0Hj-CbfmvkXgks,
+
+STRIPE_SECRET_KEY = sk_test_51OzZPkSAPPq3vrauNvual4LWl0YVrzrQYqQ4F7sVOYGZ7OJZTOBzG6njLMjkVWvDtVCsDDGsfHfgCG0W6sNQRqWm008MSoifUM,
+
+
+CORS_URL = https://elysian-book-client.vercel.app,
+// # CORS_URL = http://localhost:5000,
 },
 };
--- src/infrastucture/config/connectDb.ts
+++ src/infrastucture/config/connectDb.ts
@@ -4,6 +4,7 @@ import mongoose from 'mongoose'
 export const connectDb = async() =>{
     try {
         const mongo_uri = process.env.MONGO_URI as string
+        console.log(mongo_uri)
         await mongoose.connect(mongo_uri)
         console.log('connected to db');
         
