9564123d9ff98f582c712dd8fcca7be4cacefaef
Aparna <manikandanaparna18@gmail.com>
2024-04-25 11:12:31 +0530

    Fix(dist folder):dist removed

--- pm2.config.js
+++ pm2.config.js
@@ -1,27 +1,18 @@
 module.exports = {
-    script: 'dist/server.js', // Path to your compiled JavaScript file
-    watch: true, // Auto-restart the app when files change (optional)
-    ignore_watch: ['node_modules','logs'], 
-    env: {
-        Node_ENV: 'production',
-        PORT: 3000,
-        MONGO_URI='mongodb+srv://aparnapie2547:ElysianBook@elysianbook.ntkhmlz.mongodb.net/ElysianBook?retryWrites=true&w=majority',
-EMAIL_PASS = oieg anhq chqh qemz,
-
-Node_ENV = production,
-
-JWT_KEY = jwt123,
-REFRESH_TOKEN_SECRET=refresh123,
-
-
-CLOUDINARY_CLOUD=dr2avntwv,
-CLOUDINARY_API_KEY=829874218365388,
-CLOUDINARY_API_SECRET=cZ_7TCeKD4mJf0Hj-CbfmvkXgks,
-
-STRIPE_SECRET_KEY = sk_test_51OzZPkSAPPq3vrauNvual4LWl0YVrzrQYqQ4F7sVOYGZ7OJZTOBzG6njLMjkVWvDtVCsDDGsfHfgCG0W6sNQRqWm008MSoifUM,
-
-
-CORS_URL = https://elysian-book-client.vercel.app,
-// # CORS_URL = http://localhost:5000,
-},
+    apps: [
+        {
+            name: 'backend',
+            script: './src/server.ts',
+            watch: true,
+            ignore_watch: ['node_modules', 'logs'],
+            instances: 1,
+            exec_mode: 'fork',
+            autorestart: true,
+            max_restarts: 10,
+            env: {
+                NODE_ENV: 'production',
+                PORT: 3000,
+            },
+        },
+    ],
 };
